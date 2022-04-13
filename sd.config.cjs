b9fd7345d4245f8d9ccd9459e1315c3b3af8deee
Joren Broekema <joren.broekema@gmail.com>
2022-04-13 11:41:10 +0200

    chore: part one of cleanup (#9)
    * chore: part one of cleanup
    
    * chore: sd-output folder
    
    Co-authored-by: Joren Broekema <36734656+undefined@users.noreply.github.com>

--- sd.config.cjs
+++ sd.config.cjs
@@ -1,49 +1,16 @@
-const coreAndSemanticTokens = ['color'];
-const componentTokens = ['button'];
-
-const tokenFilter = (cat) => (token) => {
-  const { category, type } = token.attributes;
-  return ['core', 'semantic'].includes(category)
-    ? type === cat
-    : category === cat;
-};
-
-const generateFilesArr = (tokensCategories, ext, format) => {
-  return tokensCategories.map((cat) => {
-    return {
-      filter: tokenFilter(cat),
-      destination: `${cat}/src/${cat}.tokens.${ext}`,
-      format,
-    };
-  });
-};
-
 module.exports = {
-  source: ['color/base.json'],
-  format: {
-    /**
-     * Add your custom formats here and
-     * use them in the platforms below, e.g.
-     *
-     * exampleFormat: (opts) => {
-     *   const { dictionary, file } = opts;
-     *   let output = '';
-     *   // do something with the dictionary, file
-     *   // and output and return it
-     *   return output;
-     * }
-     */
-  },
+  source: ['sd-input/**/*.json'],
   platforms: {
     css: {
       transformGroup: 'css',
-      prefix: 'starter',
-      buildPath: '',
-      files: generateFilesArr(
-        [...coreAndSemanticTokens, ...componentTokens],
-        'css',
-        'css/variables'
-      ),
+      prefix: 'specify',
+      buildPath: 'sd-output/',
+      files: [
+        {
+          destination: 'variables.css',
+          format: 'css/variables',
+        },
+      ],
     },
   },
 };
